d02d32040df9f6115d825c86a1669d17ba753043
lealucia <162601328+lealucia@users.noreply.github.com>
2024-04-09 14:21:26 +0200

    Popup Text geändert und Fehler in main.js behoben

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-main.js
-var map = L.map('map').setView([49.891667, 10.891667], 13); L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+
+var map = L.map('map').setView([49.891667, 10.891667], 13); 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
-var marker = L.marker([49.891667, 10.891667], 13]).addTo(map);
+var marker = L.marker([49.891667, 10.891667], 13).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am Kaikoura.").openPopup();
+marker.bindPopup("<b>Hello world!</b><br> This is Kaikoura.").openPopup();
