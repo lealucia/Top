dcfbf09d64c93fe7dd169778f22b530afcca65c0
lealucia <162601328+lealucia@users.noreply.github.com>
2024-04-16 12:57:12 +0200

    var zu let map

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ let lat = 49.891667;
 let lng = 10.891667;
 let zoom = 13;
 
-let map = L.map('map', {
-    center: [lat, lng],
-    zoom: zoom
-});
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+var marker = L.Marker([lat, lng]).addTo(map);
+marker.bindPopup("<b> Bamberg </b><br> Highlights <br>").openPopup();
+
+
 L.control.scale({
-    imperial: false
+    imperial: false,
+    maxWidth: 150 
 }).addTo(map);
 
 let jsonData = {
@@ -263,18 +266,16 @@ let jsonData = {
 }
 
 L.geoJSON(jsondata, {}).bindPopup(function (layer) {
+    console.log(layer.feature.properties)
+
 
     return `
     <h2>${layer.feature.properties.name}</h2>
     <ul>
-    <li> Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)} </li>
-    <li> Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)} </li>
-    <li> <a href="https://${layer.feature.properties.user}.github.io/nz">Etappenseite</a>  </li>
-    <li> <a href="${layer.feature.properties.wikipedia}" >Wikipediaseite </li>
-    </li>
-
-    </ul> 
-    `;
+    <li><b>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)}</b></li>
+    <li><b>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)}</b></li>
+    <li><a href="${layer.feature.properties.wikipedia}">Wikipedia</a></li>
+    <li><a href="https://${layer.feature.properties.user}.github.io/top/">User-Website</a></li>
+</ul>`;
+    
 }).addTo(map);
-
-
